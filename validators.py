3a9a69c3e28d799df98915fbae2868cdc45a1ac6
Danil Borovskiy <borovskiy.danil@bk.ru>
2026-03-02 23:58:55 +0300

    Убрал случайно затесавшийся print

--- validators.py
+++ validators.py
@@ -20,7 +20,6 @@ def parse_email(email_str: str | None) -> None:
     if email_str is not None:
         # валидация только если есть значение
         if not re.fullmatch(r'[\w.-]+@[\w.-]+\.\w+', email_str):
-            print(re.fullmatch(r'[\w.-]+@[\w.-]+\.\w+', email_str))
             raise typer.BadParameter(
                 f'Неверный формат почты: {email_str}\n'
                 f'Корректный формат: username@example.com'
