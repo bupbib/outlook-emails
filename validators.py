75adfeb69083ee71db343faeb23014fc1f884d58
Danil Borovskiy <borovskiy.danil@bk.ru>
2026-03-04 13:43:54 +0300

    Удалил validate_dates, т.к. теперь у меня даты преобразуются в callback и проверку перенес в cli.py

--- validators.py
+++ validators.py
@@ -23,17 +23,3 @@ def parse_email(email_str: str | None) -> None:
             f'Неверный формат почты: {email_str}\n'
             f'Корректный формат: username@example.com'
         )
-
-
-def validate_dates(date_from: str | None, date_to: str | None) -> None:
-    """Проверяет, что даты указаны корректно и не противоречат друг другу"""
-    if date_from is None or date_to is None:
-        return
-
-    from_dt = datetime.strptime(date_from, '%d.%m.%Y')
-    to_dt = datetime.strptime(date_to, '%d.%m.%Y')
-
-    if from_dt > to_dt:
-        raise typer.BadParameter(
-            f'Дата начала {date_from} позже даты окончания {date_to}'
-        )
