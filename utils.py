50b2e00a8f04d3438a088b4e20e6838f656352a9
Danil Borovskiy <borovskiy.danil@bk.ru>
2026-03-05 21:44:21 +0300

    Добавил функцию build_message_filter для передачи фильтра в метод emails

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 from typing import Iterator
 from win32com.client import CDispatch
+import locale
+from datetime import datetime
 from user_docs import LEXICON
-from enums import MyApp
+from enums import MyApp, EmailStatus, FlagStatus
 
 
 def get_all_folders(folders: CDispatch) -> Iterator[CDispatch]:
@@ -32,3 +34,41 @@ def generate_docs(cmd: MyApp):
         return func
 
     return decorator
+
+
+def build_message_filter(
+        status: EmailStatus,
+        sender: str | None,
+        flag: FlagStatus,
+        date_from: datetime | None,
+        date_to: datetime | None
+) -> str:
+    """
+    Функция для построения фильтра поиска сообщений.
+    """
+    conditions = []
+    locale.setlocale(locale.LC_ALL, '')
+
+    if status in (EmailStatus.READ, EmailStatus.UNREAD):
+        conditions.append('[Unread] = True' if status == EmailStatus.UNREAD else '[Unread] = False')
+
+    if sender:
+        conditions.append(f'[SenderEmailAddress] = "{sender}"')
+
+    if flag != FlagStatus.ALL:
+        conditions.append({
+            FlagStatus.ANY: '[FlagStatus] <> 0',
+            FlagStatus.NONE: '[FlagStatus] = 0',
+            FlagStatus.EXEC: '[FlagStatus] = 1',
+            FlagStatus.COMP: '[FlagStatus] = 2'
+        }.get(flag))
+
+    if date_from:
+        start = date_from.replace(hour=0, minute=0, second=0).strftime('%x %H:%M')
+        conditions.append(f'[ReceivedTime] >= "{start}"')
+
+    if date_to:
+        end = date_to.replace(hour=23, minute=59, second=59).strftime('%x %H:%M')
+        conditions.append(f'[ReceivedTime] <= "{end}"')
+
+    return ' AND '.join(conditions)
